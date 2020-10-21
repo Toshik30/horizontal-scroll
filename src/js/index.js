4e3b758a5dcf32dd75b31336c2f2b89d6c390544
User <User@DESKTOP-4G38A9P.(none)>
2020-10-21 09:47:29 +0300

    done

--- src/js/index.js
+++ src/js/index.js
@@ -16,3 +16,16 @@ window,addEventListener("scroll", () => {
     toTop.classList.remove("active");
   }
 });
+
+const smoothLinks = document.querySelectorAll('a[href^="#"]');
+for (let smoothLink of smoothLinks) {
+    smoothLink.addEventListener('click', function (e) {
+        e.preventDefault();
+        const id = smoothLink.getAttribute('href');
+
+        document.querySelector(id).scrollIntoView({
+            behavior: 'smooth',
+            block: 'start'
+        });
+    });
+};
